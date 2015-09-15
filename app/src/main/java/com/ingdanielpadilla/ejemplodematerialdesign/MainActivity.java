1d87d2929a41431d854329e969c0db191fb79bbb
Daniel Padilla <danperzipa@hotmail.com>
2015-09-15 11:20:08 -0500

    ...

--- app/src/main/java/com/ingdanielpadilla/ejemplodematerialdesign/MainActivity.java
+++ app/src/main/java/com/ingdanielpadilla/ejemplodematerialdesign/MainActivity.java
@@ -1,9 +1,13 @@
 package com.ingdanielpadilla.ejemplodematerialdesign;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar=(Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+
+        DrawerLayout mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle mdrawerToggle=new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_close);
+        mDrawerLayout.setDrawerListener(mdrawerToggle);
+        mdrawerToggle.syncState();
+
     }
 
     @Override
