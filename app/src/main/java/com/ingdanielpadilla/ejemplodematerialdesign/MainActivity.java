742064caa197c37181612cf4c0b6775694fd4370
Daniel Padilla <danperzipa@hotmail.com>
2015-09-22 10:43:12 -0500

    ....

--- app/src/main/java/com/ingdanielpadilla/ejemplodematerialdesign/MainActivity.java
+++ app/src/main/java/com/ingdanielpadilla/ejemplodematerialdesign/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ingdanielpadilla.ejemplodematerialdesign;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,8 +15,10 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-
+    String SELECTED_ITEM_ID="SELECTED_ITEM_ID";
     DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mdrawerToggle ;
+    Integer mSelectedId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +31,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mNavigationView.setNavigationItemSelectedListener(this);
 
         mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle mdrawerToggle=new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_close);
+        mdrawerToggle =new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.drawer_open,R.string.drawer_close);
         mDrawerLayout.setDrawerListener(mdrawerToggle);
         mdrawerToggle.syncState();
+
+        mSelectedId= savedInstanceState == null ? R.id.navigation_item_1 : savedInstanceState.getInt(SELECTED_ITEM_ID);
+
+        navigate(mSelectedId);
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState,PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SELECTED_ITEM_ID,mSelectedId);
 
     }
 
@@ -58,12 +73,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
+        menuItem.setChecked(true);
+        mSelectedId = menuItem.getItemId();
+        navigate(mSelectedId);
+        return true;
+    }
+
+    private void navigate(int selected){
         Intent intent=null;
-        if(menuItem.getItemId() == R.id.navigation_item_1){
+        if(selected == R.id.navigation_item_1){
             mDrawerLayout.closeDrawer(GravityCompat.START);
             intent = new Intent(this,SecondActivity.class);
             startActivity(intent);
-            return true;
-        }else{return false;}
+        }
     }
 }
